c72401f84e5d216906df2ada887dd371e395894a
areum514 <21500514@handong.edu>
2020-11-13 13:33:09 +0900

    reysavke cinoibebts wutg jsx + props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import Potato from './Potato'
+
+function Food({fav}) {
+  return (
+  <h1>I LIKE {fav}</h1>
+  );
+}
 
 function App() {
   return (
     <div className="App">
       hello
-      <Potato/>
+      <Food fav="kimch"/ >
+      <Food fav="삼겹살"/ >
+
+        
     </div>
   );
 }
