85b530ab2d60d955da2b73131770f89e3afe8e34
areum514 <21500514@handong.edu>
2020-11-13 17:41:57 +0900

    buding the navigation

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import React from "react"
 import {HashRouter,Route} from "react-router-dom"
 import About from "./routes/About"
 import Home from "./routes/Home"
-
+import Navigator from "./components/Navigator"
 function App (){
   return <HashRouter>
+    <Navigator/>
     <Route path="/" exact={true} component={Home}/>
     <Route path="/about" component={About}/>
   </HashRouter>
